47872a16d4dd8346fa6ac8105366e5bca28b1d1b
vedantvaishnav321-cpu <vedantvaishnav321@gmail.com>
2026-03-16 22:27:03 +0530

    Added pattern programs

---
+++ Patterns/pattern1.java
@@ -0,0 +1,12 @@
+class pattern1{
+    public static void main(String[] args) {
+        
+        for (int i = 1; i <= 4; i++) {
+            for (int star = 1; star <=i; star++) {
+                System.out.print("*");
+            }
+
+             System.out.println();
+        }
+    }
+}
