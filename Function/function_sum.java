afabd3fa3c9dc28af170542a60c746b4a8616a4d
vedantvaishnav321-cpu <vedantvaishnav321@gmail.com>
2026-03-18 11:57:18 +0530

    Do while programs

---
+++ Function/function_sum.java
@@ -0,0 +1,13 @@
+class function_sum {
+    
+    static void add(int num1, int num2){
+        int mul = num1 * num2;
+        System.out.println("Sum= "+mul);
+    }
+      
+    public static void main(String[] args) {
+
+       add(5,3);    
+
+    }
+}
