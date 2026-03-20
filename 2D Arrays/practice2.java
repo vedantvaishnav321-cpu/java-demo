59d23328ce61f37ed367134c871b937504b26bc3
vedantvaishnav321-cpu <vedantvaishnav321@gmail.com>
2026-03-20 13:02:53 +0530

    if-else

---
+++ 2D Arrays/practice2.java
@@ -0,0 +1,25 @@
+class practice2 {
+
+    public static void main(String[] args) {
+        
+        int arr[][] = {
+                       {1,2,3 },
+                       {4,5,6 },
+                       {7,8,9 }
+                      };
+    
+        int sum = 0;              
+    
+          for (int i = 0; i < arr.length; i++) {
+                for (int j = 0; j < arr[i].length; j++) {
+    
+                   sum = sum + arr[i][j];     
+                   System.out.println(arr[i][j] + " " );
+              }
+          }            
+    
+            System.out.println("sum= "+sum);
+    
+    
+    }                
+}
---
+++ Arrays/practice_1.java
@@ -0,0 +1,17 @@
+class practice_1{
+
+    public static void main(String[] args) {
+        
+        int arr[] = {10,20,30,40,50};
+        int max=arr[0];
+         
+         for (int i = 0; i < arr.length; i++) {
+         
+           if( arr[i] > max) {
+             max=arr[i];
+
+           }
+         }
+           System.out.println("max= "+max);
+    }
+ }
